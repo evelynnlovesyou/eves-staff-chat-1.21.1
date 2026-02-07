b6b470c6694a2bdb17cb3161d83bf2daeb70e529
evelynn <evessial@gmail.com>
2026-02-07 01:07:23 +0000

    Initial commit

---
+++ src/client/java/io/github/evelynnlovesyou/evesstaffchat/EvesStaffChatClient.java
@@ -0,0 +1,10 @@
+package io.github.evelynnlovesyou.evesstaffchat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EvesStaffChatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
